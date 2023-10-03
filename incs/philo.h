461a77fa96cb6824591978ccbfa5e39120c667c3
Larrain Gonzalez Sebastian <lagonzal@c3r8s1.42urduliz.com>
2023-10-03 12:35:31 +0200

    Restarting the project and now documenting code.

--- incs/philo.h
+++ incs/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: larra <larra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lagonzal <lagonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 13:07:21 by larra             #+#    #+#             */
-/*   Updated: 2023/08/10 20:40:43 by lagonzal         ###   ########.fr       */
+/*   Updated: 2023/10/03 12:34:36 by lagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,17 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+/*
+Param structure. This param structure saves the parameter data that is given
+when executing the binary. 
+	- philo_num:	number of philosophers to seat on the table.
+	- t_to_die:		time a philo can wait between 2 meals before dying.
+	- t_to_slp:		time a philo will spleep after eating.
+	- t_to_eat:		time a philo will take to eat. 
+	- eatend:		times all the philosophers should eat before stopping. this
+					argument is optional
+*/
+
 typedef struct s_param
 {
 	int	philo_num;
@@ -28,33 +39,55 @@ typedef struct s_param
 	int	eatend;
 }	t_param;
 
+/*
+Philo structure. This philo structure represents each of the philosophers
+that will take part in the problem. This philosophers will be nodes in a 
+circular double linked list (table from s_all).
+	- left:			a pointer to the philosopher that will be on his left in
+					the hypothetical table. 
+	- right: 		a pointer to the philosopher that will be on his right in
+					the hypothetical table.
+	- last_meal:	the time that this philo last ate in ms.
+	- meal: 		the times this philo has already eaten.
+	- pos:			the position of the philo on the table [1 - n].
+	- fork:			the fork the philosopher carries to the table.
+	- param:		a pointer to the parameters in order to check them if
+					necesary.
+	- watcher:		a pointer to the watcher structure. The watcher will tell
+					the program when to stop.
+*/
+
 typedef struct s_philo
 {
-	struct s_philo	*left;
-	struct s_philo	*right;
-	unsigned int	last_meal;
-	int				meal;
-	int				pos;
-	t_param			*param;
-	struct s_all	*all;
+	struct s_philo		*left;
+	struct s_philo		*right;
+	unsigned int		last_meal;
+	int					meal;
+	int					pos;
+	pthread_mutex_t		*fork;
+	t_param				*param;
+	struct s_watcher	*all;
 }	t_philo;
 
-typedef struct s_all
+/*
+Watcher structure. This watcher structure will be the one that will be passed
+to the watcher thread. It has access to the philo structures and to the
+parameters.
+	- params:	a pointer to the paramater structure.
+	- table:	a pointer to the table where the philos are seated.
+	- threads:	a pointer to the threads that will be launched as philosophers.
+	- start:	it will be used as a boolean to keep the program running.
+	- stop:		it will be used as a boolean to stop the program.
+*/
+
+typedef struct s_watcher
 {
-	struct s_param	param;
-	t_philo			*philos;
+	t_param			*params;
+	t_philo			*table;
 	pthread_t		*threads;
-	pthread_mutex_t	*forks;
 	char			start;
 	char			stop;
-	unsigned int	go;
 	int				dead;
-}   t_all;
-
-int 	check_args(int argc, char **argv, struct s_param *param);
-int		create_philos(t_all *all);
-int 	ft_error(int ecode);
-int		ft_free(t_all *all);
-int	thread_creation(t_all *all);
+}	t_watcher;
 
 #endif
--- src/00_main.c
+++ src/00_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: larra <larra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lagonzal <lagonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 12:53:45 by larra             #+#    #+#             */
-/*   Updated: 2023/08/10 20:50:14 by lagonzal         ###   ########.fr       */
+/*   Updated: 2023/10/03 12:02:10 by lagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,5 @@
 
 int	main(int argc, char **argv)
 {
-	struct s_all	all;
-	//struct s_param	param;
-	//int				err;
-	
-	//err = 0;
-	//all.param = param;
-	if(check_args(argc, argv, &all.param))	
-		return (ft_error(1));
-	if (create_philos(&all))
-		return (ft_error(2));
-	thread_creation(&all);
 	
 }
